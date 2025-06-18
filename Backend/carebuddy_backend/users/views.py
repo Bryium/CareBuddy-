b8a050ffde683400f94c7a5f99fb5e98ac83d02c
Bryium <bryiumonyancha@gmail.com>
2025-06-19 02:31:28 +0300

    add users views

--- Backend/carebuddy_backend/users/views.py
+++ Backend/carebuddy_backend/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import generics
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+class RegistrationView(generics.GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProfileView(generics.GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+    def get_object(self):
+        return self.request.user
