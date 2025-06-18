2b9c6b5b59285c238812f7f7bda30360b91a0f17
Bryium <bryiumonyancha@gmail.com>
2025-06-19 01:17:55 +0300

    define database models

--- Backend/carebuddy_backend/accounts/models.py
+++ Backend/carebuddy_backend/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('patient', 'Patient'),
+        ('doctor', 'Doctor'),
+        ('admin', 'Admin'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.TextField( blank=True)
