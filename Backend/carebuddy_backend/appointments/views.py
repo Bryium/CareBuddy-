fcb5be60696102c7f10e7a13eb6a0b15b55a7aa1
Bryium <bryiumonyancha@gmail.com>
2025-06-19 11:05:35 +0300

    create views for booking,reschudling and cancelling

--- Backend/carebuddy_backend/appointments/views.py
+++ Backend/carebuddy_backend/appointments/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Appointment
+from .serializers import AppointmentSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+class AppointmentList(generics.ListCreateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Appointment.objects.filter(patient=self.request.user)
+
+class AppointmentUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+    permission_classes = [IsAuthenticated]
