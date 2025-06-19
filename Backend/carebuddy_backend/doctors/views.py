3cd0208d944667cfd800fde25bb91d65ee7f42f5
Bryium <bryiumonyancha@gmail.com>
2025-06-19 10:55:35 +0300

    create views for CRUD operations

--- Backend/carebuddy_backend/doctors/views.py
+++ Backend/carebuddy_backend/doctors/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Doctor
+from .serializers import DoctorSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Create your views here.
+class DoctorListCreateView(generics.ListCreateAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    permission_classes = (IsAuthenticated, IsAdminUser)
+
+
+class DoctorRetrieveupdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    permission_classes = (IsAuthenticated, IsAdminUser)
